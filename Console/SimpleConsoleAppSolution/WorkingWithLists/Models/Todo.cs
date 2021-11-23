e732ae8a80827a1017f43287052291b0aa4200f3
RobertasMolis <93397143+RobertasMolis@users.noreply.github.com>
2021-11-23 21:34:00 +0200

    Working With Lists

---
+++ Console/SimpleConsoleAppSolution/WorkingWithLists/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithLists.Models
+{
+    public class Todo
+    {
+        // variable/ Field
+        public string NameVariable = "";
+
+        //property
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
--- Console/SimpleConsoleAppSolution/WorkingWithLists/Program.cs
+++ Console/SimpleConsoleAppSolution/WorkingWithLists/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WorkingWithLists.Models;
 
 namespace WorkingWithLists
 {
@@ -6,7 +9,39 @@ namespace WorkingWithLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var todo1 = new Todo();
+            todo1.Name = "Clean room";
+            todo1.Description = "Just clean the room";
+            todo1.Completed = false;
+
+            var todo2 = new Todo()
+            {
+                Name = "Wash clothes",
+                Description = "Just the washing machine",
+                Completed = true
+            };
+            var todo3 = new Todo()
+            {
+                Name = "Wash clothes",
+                Description = "Just the washing machine",
+                Completed = true
+            };
+
+            var todos = new List<Todo>();
+            todos.Add(todo1);
+            todos.Add(todo2);
+            todos.Add(todo3);
+
+            // JS
+            // forEach -> forEach
+            todos.ForEach(todo => todo.Description = "");
+
+            // filter -> Where
+            todos = todos.Where(t => t.Completed == true).ToList();
+
+            // Map -> Select
+            var names = todos.Select(t => t.Name + t.Description).ToList();
+
         }
     }
 }
