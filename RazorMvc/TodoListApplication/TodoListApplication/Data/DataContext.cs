128b194f1fbadfcc024442c991b560e409d72d6a
RobertasMolis <93397143+RobertasMolis@users.noreply.github.com>
2021-12-15 19:48:56 +0200

    EF Core

---
+++ RazorMvc/TodoListApplication/TodoListApplication/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApplication.Models;
+
+namespace TodoListApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
